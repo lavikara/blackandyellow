aec4967ef8ef6e570b7b4d1c031b6f7bef4ca562
lavikara <karatemitayo@gmail.com>
2023-08-02 15:54:30 +0100

    setup store using modules

--- src/store/Index.js
+++ src/store/Index.js
@@ -1,11 +1,20 @@
 import { createStore } from "vuex";
+import state from "./state";
+import * as getters from "./getters";
+import * as mutations from "./mutations";
+import * as actions from "./actions";
+import taskModule from "./taskModule/index";
 
 const store = createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  // state: {},
+  // getters: {},
+  // mutations: {},
+  // actions: {},
+  state,
+  getters,
+  mutations,
+  actions,
+  modules: { taskModule },
 });
 
 export default store;
