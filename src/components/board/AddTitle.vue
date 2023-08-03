bb8b116acc97945331c4bc5e0413f77b97b69327
lavikara <karatemitayo@gmail.com>
2023-08-03 21:20:22 +0100

    implement add title component

---
+++ src/components/board/AddTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="add-title">
+    <div
+      class="container tw-flex tw-items-center tw-rounded-lg tw-shadow tw-cursor-pointer tw-p-2"
+      @click="emit('clicked')"
+    >
+      <PlusIcon class="tw-mr-2" />
+      <p class="tw-text-sm">Add Title</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import PlusIcon from "@/components/icons/PlusIcon.vue";
+
+const emit = defineEmits(["clicked"]);
+</script>
+
+<style lang="scss" scoped>
+#add-title {
+  @include fadeIn;
+  .container {
+    background-color: $modal-bg;
+  }
+}
+</style>
