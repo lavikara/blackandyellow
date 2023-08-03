c1024febcd1dee723b52b2369c263ff634b1cc82
lavikara <karatemitayo@gmail.com>
2023-08-03 21:37:18 +0100

    style home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <template>
-  <div id="home-view">
-    <h1>Welcome to groove task manager</h1>
+  <div
+    id="home-view"
+    class="tw-h-screen tw-flex tw-flex-col tw-justify-center tw-items-center"
+  >
+    <h1 class="tw-text-2xl">Welcome to groove task manager</h1>
+    <span
+      class="tw-text-white tw-bg-green tw-rounded-lg tw-cursor-pointer tw-p-4 tw-mt-4"
+      @click="router.push({ name: 'DefaultBoardView' })"
+      >Get Started</span
+    >
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+</script>
 
 <style lang="scss" scoped></style>
