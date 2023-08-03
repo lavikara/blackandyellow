dc9f03c981b01a8a8b9626ac9b1b74f56eac9f2e
lavikara <karatemitayo@gmail.com>
2023-08-03 21:21:51 +0100

    create btn component

---
+++ src/components/general/BtnComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    ref="btnEl"
+    contentEditable="false"
+    type="submit"
+    class="tw-text-white tw-rounded-lg tw-cursor-pointer"
+    :style="btnStyle"
+    @mouseover="setHoverStyle"
+    @mouseleave="resetStyle"
+  >
+    <span>{{ props.title }}</span>
+  </button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  title: String,
+  btnStyle: { type: Object, required: true },
+});
+
+const btnEl = ref();
+
+const setHoverStyle = () => {
+  btnEl.value.style.color = props.btnStyle.hoverColor;
+  btnEl.value.style.backgroundColor = props.btnStyle.hoverBgColor;
+};
+
+const resetStyle = () => {
+  btnEl.value.style.color = props.btnStyle.color;
+  btnEl.value.style.backgroundColor = props.btnStyle.backgroundColor;
+};
+</script>
+
+<style lang="scss" scoped></style>
