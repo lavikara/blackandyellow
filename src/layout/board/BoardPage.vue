c86fff6dbbbb91216eb165c028affbbd3dac7a17
lavikara <karatemitayo@gmail.com>
2023-08-03 21:32:19 +0100

    implement board page

---
+++ src/layout/board/BoardPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="task-page">
+    <ListContainer />
+  </div>
+</template>
+
+<script setup>
+import ListContainer from "@/components/board/ListContainer.vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+store.dispatch("taskModule/getAllTask");
+</script>
+
+<style lang="scss" scoped></style>
