9eb2a9f030aef708667ca909249d3868f0485501
lavikara <karatemitayo@gmail.com>
2023-09-14 12:18:49 +0100

    add pryme sdk

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,19 +3,33 @@
     id="home-view"
     class="tw-h-screen tw-flex tw-flex-col tw-justify-center tw-items-center"
   >
-    <h1 class="tw-text-2xl">Welcome to groove task manager</h1>
+    <h1 class="tw-text-2xl">Fund wallet with pryme bank</h1>
     <span
       class="tw-text-white tw-bg-green tw-rounded-lg tw-cursor-pointer tw-p-4 tw-mt-4"
-      @click="router.push({ name: 'DefaultBoardView' })"
-      >Get Started</span
+      @click="testSdk"
+      >Fund wallet</span
     >
   </div>
 </template>
 
 <script setup>
+import { fundWithVirtualAccount } from "@lavikara/testsdk";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+
+const testSdk = () => {
+  const details = {
+    reference: "29F83ND82JD0",
+    amount: "10000000",
+    email: "customer@gmail.com",
+    fullName: "Adewale Johnson",
+    phone: "+2348144582144",
+  };
+  fundWithVirtualAccount("apiKey", details).then((response) => {
+    console.log(response);
+  });
+};
 </script>
 
 <style lang="scss" scoped></style>
